a8e4cd15d9ef48d8deff7490d0ba000716d1fd03
Nanthini K <155549510+Nanthini-123@users.noreply.github.com>
2026-08-31 19:59:10 +0530

    Time: 15 ms (55.22%) | Memory: 47.4 MB (82.47%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+            PriorityQueue<Integer> minheap=new PriorityQueue<>((n1,n2)-> Integer.compare(map.get(n1),map.get(n2)));
+
+            for(int num:map.keySet()){
+                minheap.offer(num);
+                if(minheap.size()>k){
+                    minheap.poll();
+                }
+            }
+
+            int[]result=new int[k];
+            for(int i=0;i<k;i++){
+                result[i]=minheap.poll();
+            }
+            return result;
+
+        
+    }
+}
